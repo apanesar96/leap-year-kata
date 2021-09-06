18c7b46f7a4c7ba68948a88f2bac796b4738678e
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-09-06 13:42:47 +0100

    initial commit

---
+++ src/test/java/com/leapYear/leapYear/LeapYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leapYear.leapYear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
